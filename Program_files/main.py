d2c09132b11dba8d35d2e65602fa1e66a48c2766
Nick <alarconnick7@gmail.com>
2023-11-19 23:30:18 -0500

    Added new test commands

--- Program_files/main.py
+++ Program_files/main.py
@@ -1,5 +1,5 @@
 # Description: Main entry point for the application
-from Program_files.CLI import DatabaseCLI
+from CLI import DatabaseCLI
 
 if __name__ == "__main__":
     # Create a new database instance
